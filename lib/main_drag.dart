df85f69e0a989c4d3b5e46aaa9bebc137307decc
neo321654 <neo321654@rambler.ru>
2024-10-15 12:43:04 +0300

    fff

--- lib/main_drag.dart
+++ lib/main_drag.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Dock(
+          child: Dock<IconData>(
             items: const [
               Icons.person,
               Icons.message,
@@ -80,25 +80,35 @@ class _DockState<T> extends State<Dock<T>> {
   }
 
   List<Widget> buildList() {
-    var tempList = _items.map(widget.builder).toList();
+    var _tempList = _items.map(widget.builder).toList();
 
-    // Container(
-    //   constraints: const BoxConstraints(minWidth: 48),
-    //   height: 48,
-    //   margin: const EdgeInsets.all(8),
-    //   decoration: BoxDecoration(
-    //     borderRadius: BorderRadius.circular(8),
-    //     color: Colors.primaries[e.hashCode % Colors.primaries.length],
-    //   ),
-    //   child: Center(child: Icon(e, color: Colors.white)),
-    // );
 
-    tempList = tempList.map((e){
-      return Draggable(feedback: e,
-      child: e);
+    _tempList = _tempList.map((e) {
+      return Draggable<Widget>(
+        data: e,
+
+        feedback: e,
+        child: DragTarget<Widget>(
+            builder: (BuildContext context,candidateData,
+              rejectedData) {
+              return e;
+            },
+          onAcceptWithDetails: (data) {
+            //   var d = data;
+
+            setState(() {
+              int oldIndex = _tempList.indexOf(data.data);
+              // Меняем местами иконки
+              var curIndex = _tempList.indexOf(e);
+              var temp = _tempList[oldIndex];
+              _tempList[oldIndex] = _tempList[curIndex];
+              _tempList[curIndex] = temp;
+            });
+          },
+        ),
+      );
     }).toList();
-    
 
-    return tempList;
+    return _tempList;
   }
 }
