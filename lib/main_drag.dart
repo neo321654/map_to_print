7021e2c33120415d12f97b5f5c0fa486eee18d62
neo321654 <neo321654@rambler.ru>
2024-10-17 00:02:56 +0300

    qqqqqqqqqqqqqqqqq

--- lib/main_drag.dart
+++ lib/main_drag.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
 }
 
 /// Dock of the reorderable [items].
-class Dock<T extends Object> extends StatefulWidget {
+class Dock<T> extends StatefulWidget {
   const Dock({
     super.key,
     this.items = const [],
@@ -61,7 +61,7 @@ class Dock<T extends Object> extends StatefulWidget {
 }
 
 /// State of the [Dock] used to manipulate the [_items].
-class _DockState<T extends Object> extends State<Dock<T>> {
+class _DockState<T> extends State<Dock<T>> {
   /// [T] items being manipulated.
   late final List<T> _items = widget.items.toList();
 
@@ -75,171 +75,345 @@ class _DockState<T extends Object> extends State<Dock<T>> {
       padding: const EdgeInsets.all(4),
       child: Row(
         mainAxisSize: MainAxisSize.min,
-        children: buildList(),
+        children:
+
+        [DockItem<T>(_items.first,widget.builder)],
+        // _items.map((e){
+        //  return DockItem<T>(e,widget.builder);
+        // }).toList(),
       ),
     );
   }
 
-  List<Widget> buildList() {
-    Offset globalDragPositions = Offset.infinite;
-    final List<Widget> tempListWidgets = [];
-    bool isDragging = false;
-    Offset posTarget = Offset.zero;
-
-
-    for (int i = 0; i < _items.length; i++) {
-      final Widget widgetFromBuilder = widget.builder(_items[i]);
-
-      Draggable finalWidget = Draggable(
-        onDragStarted: (){
-          isDragging = true;
-        },
-        onDragEnd: (_){
-          isDragging = false;
-          globalDragPositions = Offset.infinite;
-        },
-
-        onDragCompleted: () {
-          isDragging = false;
-          globalDragPositions = Offset.infinite;
-        },
-        onDraggableCanceled: (_, __) {
-          isDragging = false;
-          globalDragPositions = Offset.infinite;
-        },
-        dragAnchorStrategy: (Draggable<Object> draggable, BuildContext context,
-            Offset position) {
-          final RenderBox renderObject =
-              context.findRenderObject()! as RenderBox;
-
-          if (renderObject.parentData is BoxParentData &&
-              globalDragPositions == Offset.infinite) {
-            BoxParentData parentData =
-                renderObject.parentData! as BoxParentData;
-            Offset offSet = parentData.offset;
-
-            print('_offSet!!!! === $offSet');
-            globalDragPositions = offSet;
-            print('_globalDragPositions === $globalDragPositions');
-          }
-          return renderObject.globalToLocal(position);
-        },
-        childWhenDragging: Visibility(
-          visible: false,
-          maintainSize: true,
-          maintainAnimation: true,
-          maintainState: true,
-          child: widgetFromBuilder,
-        ),
-        data: _items[i],
-        feedback: widgetFromBuilder,
-        child: DragTarget<T>(
-          builder: (BuildContext context, candidateData, rejectedData) {
-            if (candidateData.isNotEmpty) {
-/////sxsxdd
-              if(isDragging){
-                RenderBox renderBox = context.findRenderObject() as RenderBox;
-
-                BoxParentData vvv = renderBox.parent?.parentData as BoxParentData;
-                posTarget = globalDragPositions - vvv.offset;
-
-              }
-
-
-
-
-
-
-
-              // Offset localPosition =
-              //     renderBox.globalToLocal(_globalDragPositions);
-              // print('localPosition == $localPosition');
-              // renderBox.localToGlobal(renderBox.)
-              // renderBox.
-              // return  TweenAnimationBuilder<Offset>(
-              //   curve: Curves.fastLinearToSlowEaseIn ,
-              //     tween: Tween<Offset>(
-              //       begin: Offset.zero,
-              //       end: candidateData.isNotEmpty ? _globalDragPositions : Offset.zero, // Изменяем смещение
-              //     ),
-              //     duration: const Duration(milliseconds: 1300),
-              //     builder: (context, offset, child) {
-
-              // setState(
-              //       () {
-                  int curIndex = _items.indexOf(candidateData.first!);
-                  _items[curIndex] = _items[i];
-                  _items[i] = candidateData.first!;
-              //   },
-              // );
-
-              return Transform.translate(
-                offset: Offset(posTarget.dx, 0),
-                child: widgetFromBuilder,
-              );
-            }
-
-            return widgetFromBuilder;
-          },
-          onAcceptWithDetails: (data) {
-            setState(
-              () {
-                int oldIndex = i;
-                int curIndex = _items.indexOf(data.data);
-                T temp = _items[oldIndex];
-                _items[oldIndex] = _items[curIndex];
-                _items[curIndex] = temp;
-              },
-            );
-          },
-          onLeave: (data){
-
-            // setState(
-            //
-            //       () {
-            //         // _items[i-1] = data!;
-            //     // int oldIndex = i;
-            //     // int curIndex = _items.indexOf(data!);
-            //     // T temp = _items[oldIndex];
-            //     // _items[oldIndex] = _items[curIndex];
-            //     // _items[curIndex] = temp;
-            //     // int oldIndex = i;
-            //     // int curIndex = _items.indexOf(data!);
-            //     // T temp = _items[oldIndex];
-            //     // _items[oldIndex] = _items[curIndex];
-            //     // _items[curIndex] = temp;
-            //   },
-            // );
-
-            // posTarget = Offset.zero;
-            // setState(() {
-            //
-            // });
-
-            //todo need check
-            if(!isDragging){
-
-              globalDragPositions = Offset.infinite;
-
-            }
-
-          },
-
-        ),
-      );
-
-      tempListWidgets.add(finalWidget);
-    }
-
-    return tempListWidgets;
-  }
+//   List<Widget> buildList() {
+//     Offset globalDragPositions = Offset.infinite;
+//     final List<Widget> tempListWidgets = [];
+//     bool isDragging = false;
+//     Offset posTarget = Offset.zero;
+//
+//
+//     for (int i = 0; i < _items.length; i++) {
+//       final Widget widgetFromBuilder = widget.builder(_items[i]);
+//
+//       Draggable finalWidget = Draggable(
+//         onDragStarted: (){
+//           isDragging = true;
+//         },
+//         onDragEnd: (_){
+//           isDragging = false;
+//           globalDragPositions = Offset.infinite;
+//         },
+//
+//         onDragCompleted: () {
+//           isDragging = false;
+//           globalDragPositions = Offset.infinite;
+//         },
+//         onDraggableCanceled: (_, __) {
+//           isDragging = false;
+//           globalDragPositions = Offset.infinite;
+//         },
+//         dragAnchorStrategy: (Draggable<Object> draggable, BuildContext context,
+//             Offset position) {
+//           final RenderBox renderObject =
+//               context.findRenderObject()! as RenderBox;
+//
+//           if (renderObject.parentData is BoxParentData &&
+//               globalDragPositions == Offset.infinite) {
+//             BoxParentData parentData =
+//                 renderObject.parentData! as BoxParentData;
+//             Offset offSet = parentData.offset;
+//
+//             print('_offSet!!!! === $offSet');
+//             globalDragPositions = offSet;
+//             print('_globalDragPositions === $globalDragPositions');
+//           }
+//           return renderObject.globalToLocal(position);
+//         },
+//         childWhenDragging: Visibility(
+//           visible: false,
+//           maintainSize: true,
+//           maintainAnimation: true,
+//           maintainState: true,
+//           child: widgetFromBuilder,
+//         ),
+//         data: _items[i],
+//         feedback: widgetFromBuilder,
+//         child: DragTarget<T>(
+//           builder: (BuildContext context,  candidateData, rejectedData) {
+//             print('injjjjjjj');
+//
+//             if (candidateData.isNotEmpty) {
+//
+//
+//
+// /////sxsxdd
+//               if(isDragging){
+//                 RenderBox renderBox = context.findRenderObject() as RenderBox;
+//
+//                 BoxParentData vvv = renderBox.parent?.parentData as BoxParentData;
+//                 posTarget = globalDragPositions - vvv.offset;
+//
+//               }
+//
+//
+//
+//
+//
+//
+//
+//               // Offset localPosition =
+//               //     renderBox.globalToLocal(_globalDragPositions);
+//               // print('localPosition == $localPosition');
+//               // renderBox.localToGlobal(renderBox.)
+//               // renderBox.
+//               // return  TweenAnimationBuilder<Offset>(
+//               //   curve: Curves.fastLinearToSlowEaseIn ,
+//               //     tween: Tween<Offset>(
+//               //       begin: Offset.zero,
+//               //       end: candidateData.isNotEmpty ? _globalDragPositions : Offset.zero, // Изменяем смещение
+//               //     ),
+//               //     duration: const Duration(milliseconds: 1300),
+//               //     builder: (context, offset, child) {
+//
+//               // setState(
+//               //       () {
+//                   int curIndex = _items.indexOf(candidateData.first!);
+//                   _items[curIndex] = _items[i];
+//                   _items[i] = candidateData.first!;
+//               //   },
+//               // );
+//
+//               return Transform.translate(
+//                 offset: Offset(posTarget.dx, 0),
+//                 child: widgetFromBuilder,
+//               );
+//             }
+//
+//             return widgetFromBuilder;
+//           },
+//           onAcceptWithDetails: (data) {
+//             setState(
+//               () {
+//                 int oldIndex = i;
+//                 int curIndex = _items.indexOf(data.data);
+//                 T temp = _items[oldIndex];
+//                 _items[oldIndex] = _items[curIndex];
+//                 _items[curIndex] = temp;
+//               },
+//             );
+//           },
+//           onLeave: (data){
+//
+//             // setState(
+//             //
+//             //       () {
+//             //         // _items[i-1] = data!;
+//             //     // int oldIndex = i;
+//             //     // int curIndex = _items.indexOf(data!);
+//             //     // T temp = _items[oldIndex];
+//             //     // _items[oldIndex] = _items[curIndex];
+//             //     // _items[curIndex] = temp;
+//             //     // int oldIndex = i;
+//             //     // int curIndex = _items.indexOf(data!);
+//             //     // T temp = _items[oldIndex];
+//             //     // _items[oldIndex] = _items[curIndex];
+//             //     // _items[curIndex] = temp;
+//             //   },
+//             // );
+//
+//             // posTarget = Offset.zero;
+//             // setState(() {
+//             //
+//             // });
+//
+//             //todo need check
+//             if(!isDragging){
+//
+//               globalDragPositions = Offset.infinite;
+//
+//             }
+//
+//           },
+//
+//         ),
+//       );
+//
+//       tempListWidgets.add(finalWidget);
+//     }
+//
+//     return tempListWidgets;
+//   }
 }
 
-class DockListWidgets extends StatelessWidget {
-  const DockListWidgets({super.key});
+class DockItem<T> extends StatefulWidget {
+  const DockItem(this.item, this.builder, {super.key});
+  final T item;
+  final Widget Function(T) builder;
+
+
+  @override
+  State<DockItem<T>> createState() => _DockItemState<T> ();
+}
+
+class _DockItemState<T> extends State<DockItem<T>> {
+
+  bool isDragging = false;
+  late Widget widgetFromBuilder;
+  Offset offset = Offset.zero;
+
+
+@override
+  void initState() {
+    super.initState();
+    widgetFromBuilder = widget.builder(widget.item);
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Draggable(
+      data: widget.item.hashCode,
+
+
+      onDragStarted: (){
+        isDragging = true;
+      },
+      onDragEnd: (_){
+        isDragging = false;
+        // globalDragPositions = Offset.infinite;
+      },
+
+      onDragCompleted: () {
+        isDragging = false;
+        // globalDragPositions = Offset.infinite;
+      },
+      onDraggableCanceled: (_, __) {
+        isDragging = false;
+        // globalDragPositions = Offset.infinite;
+      },
+      dragAnchorStrategy: (Draggable<Object> draggable, BuildContext context,
+          Offset position) {
+        final RenderBox renderObject =
+        context.findRenderObject()! as RenderBox;
+
+        // if (renderObject.parentData is BoxParentData &&
+        //     globalDragPositions == Offset.infinite) {
+        //   BoxParentData parentData =
+        //   renderObject.parentData! as BoxParentData;
+        //   Offset offSet = parentData.offset;
+        //
+        //   print('_offSet!!!! === $offSet');
+        //   globalDragPositions = offSet;
+        //   print('_globalDragPositions === $globalDragPositions');
+        // }
+        return renderObject.globalToLocal(position);
+      },
+      childWhenDragging: Visibility(
+        visible: false,
+        maintainSize: true,
+        maintainAnimation: true,
+        maintainState: true,
+        child: widgetFromBuilder,
+      ),
+      feedback: widgetFromBuilder,
+      child: DragTarget(
+        builder: (BuildContext context, candidateData, rejectedData) {
+          print('injjjjjjj');
+
+          if (candidateData.isNotEmpty) {
+
+
+
+/////sxsxdd
+            if(isDragging){
+              RenderBox renderBox = context.findRenderObject() as RenderBox;
+
+              BoxParentData vvv = renderBox.parent?.parentData as BoxParentData;
+              // posTarget = globalDragPositions - vvv.offset;
+
+            }
+
+
+
+
+
+
+
+            // Offset localPosition =
+            //     renderBox.globalToLocal(_globalDragPositions);
+            // print('localPosition == $localPosition');
+            // renderBox.localToGlobal(renderBox.)
+            // renderBox.
+            // return  TweenAnimationBuilder<Offset>(
+            //   curve: Curves.fastLinearToSlowEaseIn ,
+            //     tween: Tween<Offset>(
+            //       begin: Offset.zero,
+            //       end: candidateData.isNotEmpty ? _globalDragPositions : Offset.zero, // Изменяем смещение
+            //     ),
+            //     duration: const Duration(milliseconds: 1300),
+            //     builder: (context, offset, child) {
+
+            // setState(
+            //       () {
+            // int curIndex = _items.indexOf(candidateData.first!);
+            // _items[curIndex] = _items[i];
+            // _items[i] = candidateData.first!;
+            //   },
+            // );
+
+            return Transform.translate(
+              offset: offset,
+              child: widgetFromBuilder,
+            );
+          }
+
+          return widgetFromBuilder;
+        },
+        onAcceptWithDetails: (data) {
+        //   setState(
+        //         () {
+        //       int oldIndex = i;
+        //       int curIndex = _items.indexOf(data.data);
+        //       T temp = _items[oldIndex];
+        //       _items[oldIndex] = _items[curIndex];
+        //       _items[curIndex] = temp;
+        //     },
+        //   );
+        },
+        onLeave: (data){
+
+          // setState(
+          //
+          //       () {
+          //         // _items[i-1] = data!;
+          //     // int oldIndex = i;
+          //     // int curIndex = _items.indexOf(data!);
+          //     // T temp = _items[oldIndex];
+          //     // _items[oldIndex] = _items[curIndex];
+          //     // _items[curIndex] = temp;
+          //     // int oldIndex = i;
+          //     // int curIndex = _items.indexOf(data!);
+          //     // T temp = _items[oldIndex];
+          //     // _items[oldIndex] = _items[curIndex];
+          //     // _items[curIndex] = temp;
+          //   },
+          // );
+
+          // posTarget = Offset.zero;
+          // setState(() {
+          //
+          // });
+
+          //todo need check
+
+
+        },
+
+      ),
+    );
   }
 }
+
+
