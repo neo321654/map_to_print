a85a21030123d0179e3ee75b6b97d202fbb7dc7b
neo321654 <neo321654@rambler.ru>
2024-10-23 11:32:41 +0300

    add new ficha 211

--- lib/pages/screen_point_to_latlng.dart
+++ lib/pages/screen_point_to_latlng.dart
@@ -496,7 +496,7 @@ List<LatLng> calculateApex({
   //fr13 13
   //fr 22
   //fr weefdfd
-  //master fdf  fdf fdfd dfdfdffdsdddssddfdf dffdf dffввава
+  //master fdf  fdf fdfd dfdfdffdsdddssddfdf dffdf dffвваывыввва
   if(landscape){
     double temp = width;
     width = height;
