22945410df9ad4019778db6947ea74bb388b1298
neo321654 <neo321654@rambler.ru>
2024-10-23 11:37:29 +0300

    вввывв

--- lib/pages/screen_point_to_latlng.dart
+++ lib/pages/screen_point_to_latlng.dart
@@ -494,7 +494,7 @@ List<LatLng> calculateApex({
   //free 222
   //fr 222
   //fr13 13ва
-  //fr 22
+  //fr 22выв
   //fr weefdfdвывыв
   //master fdf  fdf fdfd dfdfdffdsdddssddfdf dffdf dffвваывыввва
   if(landscape){
