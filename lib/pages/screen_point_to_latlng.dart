57c725a57972889adac5b46221e33469351a3919
neo321654 <neo321654@rambler.ru>
2024-10-23 14:16:23 +0300

    xfcdf

--- lib/pages/screen_point_to_latlng.dart
+++ lib/pages/screen_point_to_latlng.dart
@@ -440,7 +440,7 @@ double getMultiply({required MapCamera camera, required double meterInCm}) {
     latLngOffset = dst.offset(latLngCenter, metricDst.toDouble(), 270);
   }
 
-//sdsd/dfdf/dffd/fdf/kklkk/fdff/dsdsd
+//sdsd/dfdf/dffd/fdf/kklkk/fdff/dsdsd/dfdf
   //точка на карте в пикселях телефона
   final offsetDistance = camera.project(latLngOffset);
 
