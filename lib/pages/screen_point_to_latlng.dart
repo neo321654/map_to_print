78d1278180ceb1b00551f3a96bb30d6e8a98c926
neo321654 <neo321654@rambler.ru>
2024-10-23 10:00:00 +0300

    mas5

--- lib/pages/screen_point_to_latlng.dart
+++ lib/pages/screen_point_to_latlng.dart
@@ -488,7 +488,7 @@ List<LatLng> calculateApex({
   //fre  fdsdf 4343
   //master 1112222
   //freelanceq 3333
-  //master
+  //master 55
   if(landscape){
     double temp = width;
     width = height;
